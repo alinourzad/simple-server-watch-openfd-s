ddc97c22bd6bfd937f3e5246318d45c9913b8974
ali nourzad <alocersade@gmail.com>
2015-10-25 20:24:12 +0330

    Update iso8583.go

--- iso8583.go
+++ iso8583.go
@@ -5,4 +5,20 @@ import (
 )
 
 func analyze_iso_msg(data []byte) {
+    DumpHex(data)
+}
+
+func DumpHex(data []byte) error {
+	if !DEBUG {
+		return nil
+	}
+	for i := 0; i < len(data); i++ {
+		if (i+1)%25 != 0 {
+			fmt.Printf("%02x ", data[i])
+		} else {
+			fmt.Printf("%02x\n", data[i])
+		}
+	}
+	fmt.Printf("\n")
+	return nil
 }
